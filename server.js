39ccfa4d89a5deb9d9c2392638a56e3e210890dd
dianalynshull <dianalynshull@gmail.com>
2021-01-03 18:51:43 -0700

    created the server file and added basic server connection information

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgain');
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3030;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://workout', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+});
