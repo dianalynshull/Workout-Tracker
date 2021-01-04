58a6e25606dbb05c0679bb931d7121acc2b6c661
dianalynshull <dianalynshull@gmail.com>
2021-01-03 19:13:32 -0700

    opened server and navigated through html routes. Looked at network tab to see what api routes we attempted and logged them as comments in the apiRoutes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,3 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { Workout } = require('../models');
 
+// /workouts GET 
+
+// /workouts POST
+
+// /workouts/range GET
+
+//  /workouts/(workout)
