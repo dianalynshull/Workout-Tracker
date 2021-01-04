f2c92ee90903178bd549992e590ab8f444b9ba27
dianalynshull <dianalynshull@gmail.com>
2021-01-03 19:03:48 -0700

    fixed typo of express.router to express.Router

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const router = express.router();
+const router = express.Router();
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
